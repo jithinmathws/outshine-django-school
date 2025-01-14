74633d815ace602bc948ab696dbf14d8cf5d8d87
jithinmathws <jithinmathws@gmail.com>
2025-01-14 18:34:37 +0530

    nc

---
+++ school/settings/local.py
@@ -0,0 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-gplmrg(s$ukijr+3rv9__-3$3)5zu#zac3+y3jl3s9l0pgwuz%'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
